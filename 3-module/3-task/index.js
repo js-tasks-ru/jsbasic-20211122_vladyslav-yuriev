dc2f9c13c390c506c2dc1f6eff7b8715846ac56c
Vladounet <vladislav@mppteam.com>
2021-12-06 22:52:22 +0200

    module-3-task-3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,9 @@
 function camelize(str) {
-  // ваш код...
+  let words_arr = str.split('-');
+  let first_word = words_arr.shift();
+  let new_arr = words_arr.map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+  new_arr.unshift(first_word);
+  return new_arr.join('');
 }
