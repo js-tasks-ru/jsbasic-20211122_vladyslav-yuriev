410f623b198964aab88a6ee3a0f6530f9beb4636
Vladounet <vladislav@mppteam.com>
2021-12-22 10:37:45 +0200

    module-6 task-1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,4 +1,4 @@
-/**
+  /**
  * Компонент, который реализует таблицу
  * с возможностью удаления строк
  *
@@ -14,5 +14,34 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.elem();
+  }
+
+  elem() {
+    const table = document.createElement('table');
+    const header = document.createElement('thead');
+    const body = document.createElement('tbody');
+
+    header.innerHTML = `<tr><th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th></tr>`; 
+    table.append(header) 
+
+    const tdArr = this.rows.map(row => {
+      return `<tr><td>${row.name}</td><td>${row.age}</td><td>${row.salary}</td><td>${row.city}</td><td><button>X</button></td></tr>`;
+    });
+
+    
+    body.innerHTML = tdArr.join(''); 
+    body.addEventListener('click', this.removeRow);
+    table.append(body);
+
+    
+    return table;
+  }
+
+  removeRow(event) {
+    if (event.target.closest('button')) {
+      event.target.closest('tr').remove();
+    }
   }
 }
